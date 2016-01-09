5028c65e91889c6396183f5b88d0d5df23535cbd
Quinn Maurmann <quinn.maurmann@gmail.com>
2016-01-09 15:59:11 -0500

    Start work on gap parity cases (new solution attempt). Case 01011 tested via temp_checker.py.

--- solution.py
+++ solution.py
@@ -2,31 +2,35 @@
 Solution (in progress) to the riddle:
 
 * Adversary gives A five unordered cards out of a deck of 52
-* A gives B four ordered cards out of the five
-* B names A's remaining card
+* A chooses four cards out of the five, puts them in any order, and gives them
+    to B.
+* B must name A's remaining card
 * Find a strategy for A and B that always allows them to do this.
 
 The basic idea (in progress) is this:
 * Represent cards by numbers between 0 and 51
 * There are 24 permutations of four cards. When A passes B four cards, B has
-    to select between 48 remaining cards; thus A's task of selecting four cards
-    out of five only needs to transmit a single bit of information to B.
-* Use gaps sizes between the cards (as numbers mod 52) to locate the missing
-    card. Original idea was that A arranges that the largest gap of size <= 19
-    contains the missing card. Does not work if original cards have a gap of
-    size 19, but something like this might be workable.
+    to distinguish between 48 remaining cards. That is, independent of the four
+    cards selected by A, the ordering is enough to transmit all but ONE bit of
+    information needed by B.
+* The remaining task is to encode one bit of information in the selection task.
+    A has five choices here, but encoding one bit of information in a uniform
+    way across all (52 choose 5) is subtle.
+* Outline... TODO
 
 Run doc tests on command line with:
     python -m doctest -v solution.py
 """
 
-import itertools
+import itertools as it
+import funcy
+from collections import namedtuple
 
 # Represent cards by the numbers 0 to 51 inclusive
 
 # Represent permutations as rearrangements of indices (0, 1, 2, 3), just as it itertools
-PERMUTATIONS = tuple(itertools.permutations((0, 1, 2, 3)))
-PERM_TO_INDEX = {p: i for i, p in enumerate(PERMUTATIONS)}
+PERMS_4 = tuple(it.permutations((0, 1, 2, 3)))
+PERM_4_TO_INDEX = {p: i for i, p in enumerate(PERMS_4)}
 
 def get_permutation(cards):
     """
@@ -52,45 +56,114 @@ def apply_permutation(cards, perm):
     """
     return tuple(cards[i] for i in perm)
 
-def get_gaps(sorted_cards):
+def find_gaps(sorted_cards):
     """
     Find "gaps mod 52" between a sorted sequence of cards. Example:
-    >>> get_gaps((3, 4, 8, 12, 47))
+    >>> find_gaps((3, 4, 8, 12, 47))
     (0, 3, 3, 34, 7)
 
-    The last gap (4 in this case) "wraps around" from the last to first card.
+    The last gap (7 in this case) "wraps around" from the last to first card.
     """
     n = len(sorted_cards)
     data = [sorted_cards[i+1] - sorted_cards[i] - 1 for i in range(n-1)]
     data.append(52 + sorted_cards[0] - sorted_cards[-1] - 1)
     return tuple(data)
 
-MAX_GAP = 19
+GAP_PARITY_5_CASES = (
+    (0, 0, 0, 0, 1),
+    (0, 0, 1, 1, 1),
+    (0, 1, 0, 1, 1),
+    (1, 1, 1, 1, 1)
+)
 
-def index_of_largest_gap(gaps):
-    """
-    Find (index of) largest gap of size <= MAX_GAP (i.e. 19)
-    >>> index_of_largest_gap((0, 3, 3, 34, 7))
-    4
-    """
-    for i, gap in sorted(enumerate(gaps), key=lambda x: x[-1], reverse=True):
-        if gap <= MAX_GAP:
-            return i
-    raise ValueError("No gap of size <= MAX_GAP")
+GAP_PARITY_4_CASES = (
+    (0, 0, 1, 1),
+    (0, 1, 0, 1),
+    (1, 1, 1, 1)
+)
 
-def decode(four_cards):
+def parities(nums):
+    return tuple(n % 2 for n in nums)
+
+def rotate(sequence, step=1):
+    return sequence[step:] + sequence[:step]
+
+CaseAndRot = namedtuple("CaseAndRot", "par_case rot_num")
+
+def find_gap_parity_case(sorted_cards):
     """
-    B's strategy for decoding A's sequence of four ordered cards:
-    * sort the cards into their natural order
-    * find largest gap of size <= MAX_GAP
-    * the missing card is in this gap
-    * use the number of the original permutation to determine which card
-    TODO: test
+    Find which gap parity case is represented, as well as how many rotations
+    are needed to align the hand to the canonical representative of that case.
+    Examples:
+    >>> find_gap_parity_case((3, 4, 8, 12, 47))
+    CaseAndRot(par_case=(0, 1, 0, 1, 1), rot_num=3)
+    >>> find_gap_parity_case((3, 4, 12, 47))
+    CaseAndRot(par_case=(0, 1, 0, 1), rot_num=0)
+    """
+    n = len(sorted_cards)
+    if n == 4:
+        gap_parity_cases = GAP_PARITY_4_CASES
+    elif n == 5:
+        gap_parity_cases = GAP_PARITY_5_CASES
+    else:
+        raise ValueError("Expected sequence of length 4 or 5; got {}".format(sorted_cards))
+    gaps = find_gaps(sorted_cards)
+    parity_rotations = funcy.take(n, funcy.iterate(rotate, parities(gaps)))
+    for rot_num, par_case in enumerate(parity_rotations):
+        if par_case in gap_parity_cases:
+            return CaseAndRot(par_case=par_case, rot_num=rot_num)
+    raise ValueError("Could not align {} with any gap parity case".format(sorted_cards))
+
+# Encoding cases!
+
+def possible_cards_between(start, end, odd_offset=True):
+    if end < start:
+        end += 52
+    real_start = start + 2 if odd_offset else start + 1
+    return [x % 52 for x in range(real_start, end, 2)]
+
+def possible_cards_0101(sorted_four):
+    """
+    >>> possible_cards_0101((3, 4, 12, 47))
+    [6, 8, 10, 49, 51, 1]
+    """
+    s = sorted_four
+    gap_parities = parities(find_gaps(s))
+    if gap_parities == (0, 1, 0, 1):
+        return possible_cards_between(s[1], s[2]) + possible_cards_between(s[3], s[0])
+    elif gap_parities == (1, 0, 1, 0):
+        return possible_cards_between(s[0], s[1]) + possible_cards_between(s[2], s[3])
+    else:
+        raise ValueError("Bad gaps for 0101")
+
+def a_encode_01011(five_cards, rot_num=None): # TODO: what do I want to do about rot_num parameter?
+    if rot_num is None:
+        rot_num = find_gap_parity_case(five_cards).rot_num
+    rotated_hand = rotate(five_cards, rot_num)
+    drop_one = rotated_hand[-1] # merges the two odd gaps
+    select_four = rotated_hand[:-1]
+    sorted_four = tuple(sorted(select_four))
+    poss = possible_cards_0101(sorted_four)
+    i = poss.index(drop_one)
+    return apply_permutation(sorted_four, PERMS_4[i])
+
+PAR_4_CASE_TO_POSS_FN = {
+    (0, 0, 1, 1): None, # TODO
+    (0, 1, 0, 1): possible_cards_0101,
+    (1, 1, 1, 1): None # TODO
+}
+
+def b_decode(four_cards):
+    """
+    >>> a_encode_01011((3, 4, 8, 12, 47), 3) # (kinda annoying to provide num_rot=3)
+    (3, 4, 47, 12)
+    >>> b_decode(_)
+    8
     """
     perm = get_permutation(four_cards)
-    sorted_cards = sorted(four_cards)
-    gaps = get_gaps(sorted_cards)
-    gap_start_index = index_of_largest_gap(gaps)
-    card_start = sorted_cards[gap_start_index]
-    perm_num = PERM_TO_INDEX[perm]
-    return (card_start + 1 + perm_num) % 52
+    i = PERM_4_TO_INDEX[perm]
+    sorted_four = tuple(sorted(four_cards))
+    par_case, rot_num = find_gap_parity_case(sorted_four)
+    possible_cards_fn = PAR_4_CASE_TO_POSS_FN[par_case]
+    cards = possible_cards_fn(sorted_four)
+    return cards[i]
---
+++ temp_checker.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+import solution
+
+
+def check(a_encode, b_decode):
+    from itertools import combinations
+    problem_message = "five_cards={} (case {} of 2598960)"
+    counts = defaultdict(int)
+    for i, five_cards in enumerate(combinations(range(52), 5)):
+        par_case, rot_num = solution.find_gap_parity_case(five_cards)
+        if par_case == (0, 1, 0, 1, 1):
+            four_cards = a_encode(five_cards)
+            assert len(set(four_cards)) == 4, problem_message.format(five_cards, i)
+            assert all(c in five_cards for c in four_cards), problem_message.format(five_cards, i)
+            guess = b_decode(four_cards)
+            assert tuple(sorted(four_cards + (guess,))) == five_cards, problem_message.format(five_cards, i)
+            counts[par_case] += 1
+        if i % 10000 == 0:
+            print "checking in: i={}, five_cards={}".format(i, five_cards)
+    print "OK so far! counts: {}".format(counts)
+
+if __name__ == "__main__":
+    check(solution.a_encode_01011, solution.b_decode)
